5a11de4d6a9f252b700b2a670c8e764826c87930
Gabriel Galli <g7.galli96@gmail.com>
2018-03-02 23:51:40 -0300

    Formatted code

--- Features.py
+++ Features.py
@@ -7,16 +7,17 @@ from scipy import stats
 from time import time, strftime
 from matplotlib import pyplot as plt
 
+SAVE_ONLY = True
 NUM_OF_PAIRS = 1
 TABLE_NAME = 'datas_{}'.format(strftime('%y%m%d_%H%M%S'))
-ONLY_SAVE = True
+
 
 def main():
     executeTimeI = time()
     conn = sqlite3.connect('banco.db')
     cursor = conn.cursor()
     cursor.execute(
-      """CREATE TABLE {} (
+        """CREATE TABLE {} (
             technique TEXT,
             situation TEXT,
             kp1 INTEGER,
@@ -34,90 +35,97 @@ def main():
     )
 
     # Initiate detectors
-    # SIFT = cv2.xfeatures2d.SIFT_create()
-    # SURF = cv2.xfeatures2d.SURF_create()
+    SIFT = cv2.xfeatures2d.SIFT_create()
+    SURF = cv2.xfeatures2d.SURF_create()
     ORB = cv2.ORB.create()
-    # # KAZE = cv2.KAZE.create()
-    # AKAZE = cv2.AKAZE.create()
-    # BRISK = cv2.BRISK.create()
+    AKAZE = cv2.AKAZE.create()
+    BRISK = cv2.BRISK.create()
 
     methods = {
-        # 'SIFT': SIFT,
-        # 'SURF': SURF,
+        'SIFT': SIFT,
+        'SURF': SURF,
         'ORB': ORB,
-        # 'KAZE': KAZE,
-        # 'AKAZE': AKAZE,
-        # 'BRISK': BRISK
+        'AKAZE': AKAZE,
+        'BRISK': BRISK
     }
 
     cases = [
         'Same Object, Same Scale',
-        # 'Same Object, Different Scale',
-        # 'Different Object, Same Scale',
-        # 'Different Object, Different Scale',
+        'Same Object, Different Scale',
+        'Different Object, Same Scale',
+        'Different Object, Different Scale'
     ]
 
     for case in cases:
-      print(case)
-      for pair in range(NUM_OF_PAIRS):
-        print('Pair {}/{}'.format(pair + 1, NUM_OF_PAIRS))
-        img1 = cv2.imread('photos/{}/{}a.jpg'.format(case,pair),0)
-        img2 = cv2.imread('photos/{}/{}b.jpg'.format(case,pair),0)
-        for name, method in methods.items():
-            print(name)
-            print("Phase One: Compares unaltered images")
-            angles_dif,scales,kp1,kp2,matches,original_values = prep_values(img1,img2,method,name,case,pair)
-            original_values.append(1)
+        print(case)
+        for pair in range(NUM_OF_PAIRS):
+            print('Pair {}/{}'.format(pair + 1, NUM_OF_PAIRS))
+            img1 = cv2.imread('photos/{}/{}a.jpg'.format(case, pair), 0)
+            img2 = cv2.imread('photos/{}/{}b.jpg'.format(case, pair), 0)
+            for name, method in methods.items():
+                print(name)
+                print("Phase One: Compares unaltered images")
+                angles_dif, scales, kp1, kp2, matches, original_values = prep_values(
+                    img1, img2, method, name, case, pair)
+                original_values.append(1)
 
-            result = cv2.drawMatches(img1,kp1,img2,kp2,matches,outImg=None)
+                result = cv2.drawMatches(
+                    img1, kp1, img2, kp2, matches, outImg=None)
 
-            save(conn, cursor,tuple(original_values))
-            plot_matches(result,ONLY_SAVE,'results/{}/{}_p1.png'.format(case,pair))
+                save(conn, cursor, tuple(original_values))
+                plot_matches(result, SAVE_ONLY,
+                             'results/{}/{}_p1.png'.format(case, pair))
 
-            print('Phase two: Calculates the transformation')
-            mean_angles = original_values[4]
-            mean_scale = original_values[6]
-            dst = gmt.affine_trans(img1,mean_angles,mean_scale)
+                print('Phase two: Calculates the transformation')
+                mean_angles = original_values[4]
+                mean_scale = original_values[6]
+                dst = gmt.affine_trans(img1, mean_angles, mean_scale)
 
-            _,_,kp1,kp2,matches,values = prep_values(dst,img2,method,name,case,pair)
-            values.append(2)
+                _, _, kp1, kp2, matches, values = prep_values(
+                    dst, img2, method, name, case, pair)
+                values.append(2)
 
-            result = cv2.drawMatches(dst,kp1,img2,kp2,matches,outImg=None)
+                result = cv2.drawMatches(
+                    dst, kp1, img2, kp2, matches, outImg=None)
 
-            save(conn, cursor,tuple(values))
-            plot_matches(result,ONLY_SAVE,'results/{}/{}_p2.png'.format(case,pair))
+                save(conn, cursor, tuple(values))
+                plot_matches(result, SAVE_ONLY,
+                             'results/{}/{}_p2.png'.format(case, pair))
 
-            print("Phase three: Removes fake matches")
-            mean_angles = original_values[4]
-            angles_std = original_values[5]
-            mean_scale = original_values[6]
-            scale_std = original_values[7]
+                print("Phase three: Removes fake matches")
+                mean_angles = original_values[4]
+                angles_std = original_values[5]
+                mean_scale = original_values[6]
+                scale_std = original_values[7]
 
-            angles_dif,scales = gmt.remove_fake_matches(matches,angles_dif,mean_angles,angles_std,scales,mean_scale,scale_std)
+                angles_dif, scales = gmt.remove_fake_matches(
+                    matches, angles_dif, mean_angles, angles_std, scales, mean_scale, scale_std)
 
-            mean_angles = stats.tstd(angles_dif)
-            angles_std = stats.tstd(angles_dif)
-            mean_scale = stats.tmean(scales)
-            scale_std = stats.tstd(scales)
+                mean_angles = stats.tstd(angles_dif)
+                angles_std = stats.tstd(angles_dif)
+                mean_scale = stats.tmean(scales)
+                scale_std = stats.tstd(scales)
 
-            dst = gmt.affine_trans(img1,mean_angles,mean_scale)
-            # ploting_image_pair(dst,img2)
-            _,_,kp1,kp2,matches,values = prep_values(dst,img2,method,name,case,pair)
+                dst = gmt.affine_trans(img1, mean_angles, mean_scale)
+                _, _, kp1, kp2, matches, values = prep_values(
+                    dst, img2, method, name, case, pair)
+                values.append(3)
 
-            values.append(3)
-            result = cv2.drawMatches(dst,kp1,img2,kp2,matches,outImg=None)
+                result = cv2.drawMatches(
+                    dst, kp1, img2, kp2, matches, outImg=None)
 
-            save(conn, cursor,tuple(values))
-            plot_matches(result,ONLY_SAVE,'results/{}/{}_p3.png'.format(case,pair))
+                save(conn, cursor, tuple(values))
+                plot_matches(result, SAVE_ONLY,
+                             'results/{}/{}_p3.png'.format(case, pair))
 
-        del img1
-        del img2
+            del img1
+            del img2
     conn.close()
     executeTimeF = time()
     print('Test executed in {} seconds'.format(executeTimeF-executeTimeI))
 
 
-def getStats(method,img1, img2):
+def getStats(method, img1, img2):
     timeI = time()
     # find the keypoints and descriptors with ORB
     kp1, des1 = method.detectAndCompute(img1, None)
@@ -133,32 +141,33 @@ def getStats(method,img1, img2):
     # Sort them in the order of their distance.
     # matches = sorted(matches, key=lambda x: x.distance)
 
-    return [kp1,kp2, matches, timeF - timeI]
+    return [kp1, kp2, matches, timeF - timeI]
 
-def save(conn,cursor,values):
-    cursor.execute("""
-      INSERT INTO {} (kp1,kp2,matches,time,anglesMean,anglesSD,scaleMean,scaleSD,technique,situation,pathImg1,pathImg2,phase)
-      VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
-      """.format(TABLE_NAME), values)
+
+def save(conn, cursor, values):
+    cursor.execute(
+        """INSERT INTO {} (kp1, kp2, matches, time, anglesMean, anglesSD, scaleMean, scaleSD, technique, situation, pathImg1, pathImg2, phase)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""".format(TABLE_NAME), values)
     conn.commit()
 
-def plot_matches(img,option,path):
-    if option == ONLY_SAVE :
-        cv2.imwrite(path,img)
+
+def plot_matches(img, option, path):
+    if option == SAVE_ONLY:
+        cv2.imwrite(path, img)
     else:
         plt.imshow(img)
         plt.show()
 
 
-def prep_values(img1,img2,method,name,case,pair):
-    values = getStats(method,img1,img2)
-    kp1,kp2,matches = values[0],values[1],values[2]
-    values[0],values[1],values[2] = len(kp1),len(kp2),len(matches)
+def prep_values(img1, img2, method, name, case, pair):
+    values = getStats(method, img1, img2)
+    kp1, kp2, matches = values[0], values[1], values[2]
+    values[0], values[1], values[2] = len(kp1), len(kp2), len(matches)
 
-    angles_img1 = gmt.g_find_kp_angles(img1,kp1)
-    angles_img2 = gmt.g_find_kp_angles(img2,kp2)
-    angles_dif = gmt.angles_dif(angles_img1,angles_img2,matches)
-    scales =  gmt.find_scale_ratios(img1, kp1, img2, kp2, matches)
+    angles_img1 = gmt.g_find_kp_angles(img1, kp1)
+    angles_img2 = gmt.g_find_kp_angles(img2, kp2)
+    angles_dif = gmt.angles_dif(angles_img1, angles_img2, matches)
+    scales = gmt.find_scale_ratios(img1, kp1, img2, kp2, matches)
 
     mean_angles = stats.tstd(angles_dif)
     std_angles = stats.tstd(angles_dif)
@@ -175,7 +184,8 @@ def prep_values(img1,img2,method,name,case,pair):
     values.append('{}a.jpg'.format(pair))
     values.append('{}b.jpg'.format(pair))
 
-    return angles_dif,scales,kp1,kp2,matches, values
+    return angles_dif, scales, kp1, kp2, matches, values
+
 
 if(__name__ == '__main__'):
     main()
--- geometry.py
+++ geometry.py
@@ -6,6 +6,7 @@ import cv2
 def image_center(image):
     return Point(image.shape[1] / 2, image.shape[0] / 2)
 
+
 # Finds the angles between the horizontal axis
 # and the lines passing through the image center
 # and each keypoint
@@ -19,21 +20,22 @@ def g_find_kp_angles(image, kps):
         angles.append(float(h_axis.angle_between(kp_line)))
     return angles
 
+
 def angles_dif(angles_img1, angles_img2, matches):
     dif = []
-    for match in matches :
+    for match in matches:
         dif.append(angles_img1[match.queryIdx] - angles_img2[match.trainIdx])
 
     return dif
 
-def remove_fake_matches(matches,dif_angles,mean_angles,angles_std,scales,mean_scale,scale_std):
-    new_scales,new_dif_angles = [],[]
+
+def remove_fake_matches(matches, dif_angles, mean_angles, angles_std, scales, mean_scale, scale_std):
+    new_scales, new_dif_angles = [], []
     for i in range(len(matches)):
         if dif_angles[i] < mean_angles + angles_std and dif_angles[i] > mean_angles - angles_std and scales[i] < mean_scale + scale_std and scales[i] > mean_angles - scale_std:
             new_scales.append(scales[i])
             new_dif_angles.append(dif_angles[i])
-    return new_dif_angles,new_scales
-
+    return new_dif_angles, new_scales
 
 
 # Finds the Key's points Angles
@@ -47,6 +49,7 @@ def find_kp_angles(kp1, kp2, matches, center1, center2):
         angles.append(float(central_line.angle_between(match_line)))
     return angles
 
+
 def g_find_scale(image, kps):
     scale = []
     center = image_center(image)
@@ -55,6 +58,7 @@ def g_find_scale(image, kps):
         d = center.distance(p)
         scale.append(d)
     return scale
+
 
 # Finds the ratio of the keypoints scale between images
 def find_scale_ratios(img1, kp1, img2, kp2, matches):
@@ -68,6 +72,7 @@ def find_scale_ratios(img1, kp1, img2, kp2, matches):
         ratios.append(float(d1 / d2))
     return ratios
 
+
 # Finds the Scale between images
 def find_scale(kp1, kp2, matches, center1, center2):
     scale = []
@@ -79,7 +84,8 @@ def find_scale(kp1, kp2, matches, center1, center2):
         scale.append(float(d1 / d2))
     return scale
 
-def affine_trans(img,angles,scale):
+
+def affine_trans(img, angles, scale):
     center = image_center(img)
     m = cv2.getRotationMatrix2D((center.y, center.x), angles, scale)
-    return cv2.warpAffine(img, m, (img.shape[1],img.shape[0]))
+    return cv2.warpAffine(img, m, (img.shape[1], img.shape[0]))
