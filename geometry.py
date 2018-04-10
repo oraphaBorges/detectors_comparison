f99ca10a2d196a1f2101da9eed2810d118b1fa90
Gabriel Galli <g7.galli96@gmail.com>
2018-04-10 10:57:41 -0300

    Just modularized angle calculation

--- geometry.py
+++ geometry.py
@@ -3,15 +3,21 @@ import numpy as np
 
 
 def eucl_distance(p1, p2):
-  return np.sqrt(np.square(p2[0] - p1[0]) + np.square(p2[1] - p1[1]))
+    return np.sqrt(np.square(p2[0] - p1[0]) + np.square(p2[1] - p1[1]))
 
 
 # Returns the vector p1 -> p2
 def points_to_vec(p1, p2):
-  v = np.empty(shape=2)
-  v[0] = p2[0] - p1[0]
-  v[1] = p2[1] - p1[1]
-  return v
+    v = np.empty(shape=2)
+    v[0] = p2[0] - p1[0]
+    v[1] = p2[1] - p1[1]
+    return v
+
+
+def vecs_angle(v1, v2):
+    # cos(theta) = (u . v) / (|u| * |v|)
+    cos_ang = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    return np.arccos(cos_ang)
 
 # The mean of the coordinates
 # of the keypoints is considered
@@ -19,13 +25,13 @@ def points_to_vec(p1, p2):
 
 
 def kps_center(kps):
-  mean = np.zeros(2)
-  for kp in kps:
-    mean[0] += kp.pt[0]
-    mean[1] += kp.pt[1]
-  mean[0] /= len(kps)
-  mean[1] /= len(kps)
-  return mean
+    mean = np.zeros(2)
+    for kp in kps:
+        mean[0] += kp.pt[0]
+        mean[1] += kp.pt[1]
+    mean[0] /= len(kps)
+    mean[1] /= len(kps)
+    return mean
 
 # Finds the angles between the horizontal axis
 # and the lines passing through the center of
@@ -33,31 +39,28 @@ def kps_center(kps):
 
 
 def find_kp_angles(center, kps):
-  angles = np.empty(shape=len(kps))
-  center_shifted = np.copy(center)
-  center_shifted[0] += center[0]
-  h_axis = points_to_vec(center, center_shifted)
-  i = 0
-  for kp in kps:
-    kp_vec = points_to_vec(center, kp.pt)
-    # cos(theta) = (u . v) / (|u| * |v|)
-    angles[i] = np.dot(h_axis, kp_vec) / \
-        (np.linalg.norm(h_axis) * np.linalg.norm(kp_vec))
-    angles[i] = np.rad2deg(np.arccos(angles[i]))
-    i += 1
-  return angles  # maintains original kps order
+    angles = np.empty(shape=len(kps))
+    center_shifted = np.copy(center)
+    center_shifted[0] += center[0]
+    h_axis = points_to_vec(center, center_shifted)
+    i = 0
+    for kp in kps:
+        kp_vec = points_to_vec(center, kp.pt)
+        angles[i] = np.rad2deg(vecs_angle(h_axis, kp_vec))
+        i += 1
+    return angles  # maintains original kps order
 
 
 def find_kps_dist(center, kps):
-  scale = np.empty(shape=len(kps))
-  i = 0
-  for kp in kps:
-    scale[i] = eucl_distance(center, kp.pt)
-    i += 1
-  return scale  # maintains original kps order
+    scale = np.empty(shape=len(kps))
+    i = 0
+    for kp in kps:
+        scale[i] = eucl_distance(center, kp.pt)
+        i += 1
+    return scale  # maintains original kps order
 
 
 def norm_angles(angles):
-  for i in range(len(angles)):
-    if angles[i] > 180.0:
-      angles[i] = 360.0 - angles[i]
+    for i in range(len(angles)):
+        if angles[i] > 180.0:
+            angles[i] = 360.0 - angles[i]
